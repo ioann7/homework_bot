35f17e54bf0e047f5c75bff04e50f85c5a2e90d9
chakchakk <ioann.chimrov@gmail.com>
2022-10-30 18:44:03 +0300

    change some funcs

--- homework.py
+++ homework.py
@@ -1,11 +1,21 @@
-...
+import os
+import time
+import logging
+from typing import Union
+
+import telegram
+import requests
+from dotenv import load_dotenv
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+logger = logging.getLogger(__name__)
+
+
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -19,15 +29,32 @@ HOMEWORK_STATUSES = {
 }
 
 
-def send_message(bot, message):
-    ...
+TApiAnswer = dict[str, Union[list, int]]
 
 
-def get_api_answer(current_timestamp):
+def send_message(bot: telegram.Bot, message: str) -> None:
+    """
+    Sends message to Telegram chat,
+    defined by environment variable `TELEGRAM_CHAT_ID`.
+    """
+    bot.send_message(message, TELEGRAM_CHAT_ID)
+    logger.info(f'Бот отправил сообщение {message}')
+
+
+def get_api_answer(current_timestamp: Union[int, str]) -> TApiAnswer:
+    """Makes a request to yandex homework API."""
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
 
-    ...
+    try:
+        response = requests.get(
+            ENDPOINT,
+            headers=HEADERS,
+            params=params
+        ).json()
+    except Exception as e:
+        pass
+    return response
 
 
 def check_response(response):
@@ -49,7 +76,10 @@ def parse_status(homework):
 
 
 def check_tokens():
-    ...
+    """Checks the availability of environment variables."""
+    if None in (PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID):
+        return False
+    return True
 
 
 def main():
